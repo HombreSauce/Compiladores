8d38370e74e7bc703098b83747dba56208db730c
Joaco <joaquingonzalez.tesoro@gmail.com>
2025-09-20 19:29:52 -0300

    hice la acción semántica 4

--- datos/MatrizAccionSemantica.java
+++ datos/MatrizAccionSemantica.java
@@ -9,7 +9,7 @@ public class MatrizAccionSemantica {
     private static final AccionSemantica AS1 = new AccionSemantica1();
     private static final AccionSemantica AS2 = new AccionSemantica2();
     private static final AccionSemantica AS3 = new AccionSemantica3(tablaSimbolos, tablaIDToken);
-    private static final AccionSemantica AS4 = new AccionSemantica1();
+    private static final AccionSemantica AS4 = new AccionSemantica4(tablaIDToken);
     private static final AccionSemantica AS5 = new AccionSemantica1();
     private static final AccionSemantica AS6 = new AccionSemantica1();
     private static final AccionSemantica AS7 = new AccionSemantica1();
